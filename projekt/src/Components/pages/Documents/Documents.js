534edc784ce634a1362909eab54ab85e015630ff
Bartek <stebart@tlen.pl>
2022-12-08 02:07:58 +0100

    Finished gird section and start Documentation

--- projekt/src/Components/pages/Documents/Documents.js
+++ projekt/src/Components/pages/Documents/Documents.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styleDocuments from "../Documents/Documents.module.css";
 import { Layout } from "../../Layout/Layout";
 
 const Documents = () => {
+  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
   return (
     <Layout>
-      <div className={styleDocuments.documents__section}>DD</div>
+      <div className={styleDocuments.documents__section}>
+        <form>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <input
+            type="file"
+            value={selectedFile}
+            onChange={(e) => setSelectedFile(e.target.files[0])}
+          />
+        </form>
+      </div>
     </Layout>
   );
 };
--- projekt/src/Components/pages/Information/Information.js
+++ projekt/src/Components/pages/Information/Information.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styleInformation from "../Information/Information.module.css";
 import { Layout } from "../../Layout/Layout";
 
 const Information = () => {
+  useEffect(() => {}, []);
+
+  const farmerPage = () => {
+    window.open("https://www.farmer.pl/");
+  };
+  const farmPage = () => {
+    window.open("https://www.wrp.pl/");
+  };
+  const agroPage = () => {
+    window.open("https://www.agropolska.pl/aktualnosci/polska/");
+  };
+  const infoPage = () => {
+    window.open("https://wiescirolnicze.pl/");
+  };
+  const agroProfilPage = () => {
+    window.open("https://agroprofil.pl/");
+  };
+  const stockMarketPage = () => {
+    window.open("https://igrit.pl/");
+  };
+
   return (
     <Layout>
       <div className={styleInformation.information__section}>
-        <p style={{ textAlign: "center" }}>Wiadomości ze świata</p>
+        <p className={styleInformation.information__title}>
+          Wiadomości z kraju
+        </p>
         <div className={styleInformation.information__grid}>
-          <div className={styleInformation.green}>
-            <p>1</p>
+          <div className={styleInformation.one} onClick={() => farmerPage()}>
+            <div className={styleInformation.information__one_img}></div>
           </div>
-          <div className={styleInformation.green}>
-            <p>2222</p>2
+          <div className={styleInformation.two} onClick={() => farmPage()}>
+            <div className={styleInformation.information__two_img}></div>
           </div>
-          <div className={styleInformation.green}>3</div>
-          <div className={styleInformation.yellow}>4</div>
-          <div className={styleInformation.green}>5</div>
-          <div className={styleInformation.black}>6</div>
-          <div className={styleInformation.green}>7</div>
-          <div className={styleInformation.green}>8</div>
-          <div className={styleInformation.pink}>9</div>
+          <div className={styleInformation.three} onClick={() => agroPage()}>
+            <div className={styleInformation.information__three_img}></div>
+          </div>
+          <div className={styleInformation.four} onClick={() => infoPage()}>
+            <div className={styleInformation.information__four_img}></div>
+          </div>
+          <div
+            className={styleInformation.five}
+            onClick={() => agroProfilPage()}
+          >
+            <div className={styleInformation.information__five_img}></div>
+          </div>
+          <div
+            className={styleInformation.six}
+            onClick={() => stockMarketPage()}
+          >
+            <div className={styleInformation.information__six_img}></div>
+          </div>
         </div>
       </div>
     </Layout>
